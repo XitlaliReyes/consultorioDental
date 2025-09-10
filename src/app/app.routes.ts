1b91d85d5dc9cb4bf6fa67c2044a572b89b0c03f
XitlaliReyes <al262973@edu.uaa.mx>
2025-09-10 10:11:16 -0600

    cambios
    diseño

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/public/home/home';
 import { Nosotros } from './pages/public/nosotros/nosotros';
+import { Calendario } from './pages/citas/calendario/calendario';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: Home },
-    { path: 'nosotros', component: Nosotros }
+    { path: 'nosotros', component: Nosotros },
+    { path: 'citas', component: Calendario}
 ];
